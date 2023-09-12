e0dadf5d3dc2ae1a04d604547772fc0c58310ad6
Infin1teFour <infin1tefour44@gmail.com>
2023-09-12 08:01:24 +0200

    dodane komentarze do koperta.py

--- Python/koperta.py
+++ Python/koperta.py
@@ -2,31 +2,38 @@ import turtle
 import math
 import random
 
+#ustawienia turtle
 t = turtle.Turtle()
 t.speed(0)
 turtle.colormode(255)
 
+#funkcja na przekątną
 def przekatna(size):
     t.forward(math.sqrt((size**2)*2))
     t.left(135)
     t.forward(size)
 
+#Główna funkcja
 def koperta(size):
+    #Kwadrat
     for i in range(4):
         t.forward(size)
         t.left(90)
 
+    #Przekątne
     t.left(45)
     przekatna(size)
     t.left(135)
     przekatna(size)
 
+    #Trójkąt na górze
     t.left(30)
     t.forward(size)
     t.left(120)
     t.forward(size)
     t.left(120)
 
+#losowanie 10 kopert
 for i in range(10):
     t.color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
     t.penup()
