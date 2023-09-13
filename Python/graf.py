d84bc9e78e0f5679705f8e711ef131ec25bb8710
Infin1teFour <infin1tefour44@gmail.com>
2023-09-13 12:18:39 +0200

    pog graf.py

--- Python/graf.py
+++ Python/graf.py
@@ -1,25 +1,14 @@
 import tkinter as tk
 
-zgoda = 0
-niezgoda = 0
-
 window = tk.Tk()
-window.geometry("400x400")
-window.resizable(False,True)
-window.title("Rozmiar Okna Imie I Nazwisko")
+window.geometry("300x100")
+window.resizable(False,False)
+window.title("Jan Jakowicki")
 
-def hello(tekst):
-    global zgoda
-    global niezgoda
-    if tekst == "Zgadzam sie":
-        zgoda = zgoda + 1
-    if tekst == "Nie zgadzam sie":
-        niezgoda = niezgoda + 1
-    print(tekst)
-    print("Ilość zgód: ",zgoda)
-    print("Ilość niezgód: ",niezgoda,"\n")
+tekst = tk.Label(text="Praca z tkinter",fg="blue").grid(column=0, row=0, sticky='w')
 
+button1 = tk.Button(window, text="Ok", padx=40, pady=30).grid(column=0, row=1, sticky='w')
+button2 = tk.Button(window, text="Nic", padx=40, pady=30).grid(column=1, row=1, sticky='w')
+button3 = tk.Button(window, text="+", padx=40, pady=30).grid(column=2, row=1, sticky='w')
 
-button1 = tk.Button(window, text="Yes", command=lambda:hello('Zgadzam sie')).pack()
-button2 = tk.Button(window, text="No", command=lambda:hello('Nie zgadzam sie')).pack()
 window.mainloop()
