297bff0e60c5928364f0be35be53c2731a85712b
Infin1teFour <infin1tefour44@gmail.com>
2023-09-19 08:22:00 +0200

    added comments

--- Python/calc.py
+++ Python/calc.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import operator
 
+#Operator lookup table
 ops = {
     "+": operator.add,
     "-": operator.sub,
@@ -14,26 +15,32 @@ ops = {
 
 root = tk.Tk()
 
+#Variables used for entry fields
 num1 = tk.StringVar()
 num2 = tk.StringVar()
 
+#Main calculate function
 def calculate(symbol):
     x = int(num1.get())
     y = int(num2.get())
     Result.config(text=(x,symbol,y,"=",ops[symbol](x,y)))
     
+#1st row, entry and result
 field1 = tk.Entry(textvariable=num1).grid(column=0, row=0)
 Result = tk.Label(text="")
 Result.grid(column=1, row=0)
 field2 = tk.Entry(textvariable=num2).grid(column=2, row=0)
 
+#2nd row
 addition = tk.Button(text="+", command=lambda: calculate("+"),padx=60, pady=20).grid(column=0, row=1)
 subtraction = tk.Button(text="-", command=lambda: calculate("-"),padx=60, pady=20).grid(column=1, row=1)
 multiplication = tk.Button(text="*",command=lambda: calculate("*"),padx=60, pady=20).grid(column=2, row=1)
 
+#3rd row
 power = tk.Button(text="**",command=lambda: calculate("**"),padx=60, pady=20).grid(column=0, row=2)
 division = tk.Button(text="/",command=lambda: calculate("/"),padx=60, pady=20).grid(column=1, row=2)
 whole_division = tk.Button(text="//",command=lambda: calculate("//"),padx=60, pady=20).grid(column=2, row=2)
 
+#4th row
 modulo = tk.Button(text="%",command=lambda: calculate("%"),padx=60, pady=20).grid(column=1, row=3)
 tk.mainloop()
